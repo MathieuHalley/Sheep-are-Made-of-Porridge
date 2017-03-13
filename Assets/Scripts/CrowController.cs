02ff4c42db5af65b800c6dbe963279fd2567dfab
Mathieu Halley <mathieu.halley@gmail.com>
2017-03-13 18:52:01 +1000

    Merge pt2

--- Assets/Scripts/CrowController.cs
+++ Assets/Scripts/CrowController.cs
@@ -11,6 +11,8 @@ public class CrowController : MonoBehaviour
 	private CrowControllerData Data { get { return _data; } }
 	private Vector2 CurPosition { get { return (Vector2)this.gameObject.transform.position; } }
 
+	public float VelocityMagnitude;
+
 	private void Awake()
 	{
 		_rigidbody = GetComponent<Rigidbody2D>();
@@ -54,12 +56,10 @@ public class CrowController : MonoBehaviour
 
 	private void MoveTowards(Vector2 target)
 	{
-		if ((target - CurPosition).magnitude > Data.MovementParameters.MaxVelocity)
-			target = new Vector2(target.x, Data.Nest.y);
-
-		_rigidbody.AddForce(target - CurPosition, ForceMode2D.Impulse);
-		_rigidbody.velocity = Vector2.ClampMagnitude(
-			_rigidbody.velocity, 
-			Data.MovementParameters.MaxVelocity);
+		MovementParameters movementParams = Data.MovementParameters;
+		float maxVelocity = movementParams.MaxVelocity;
+		Vector2 movementDistance = target - CurPosition;
+		movementDistance = Vector2.ClampMagnitude(movementDistance, maxVelocity * Time.fixedDeltaTime);
+		_rigidbody.MovePosition(movementDistance + CurPosition);
 	}
 }
